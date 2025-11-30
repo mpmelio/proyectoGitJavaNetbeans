d44e4c6d8b147e96b0945eb4148bfafb527f8f42
mpmelio <mp.meliogonzalvez@edu.gva.es>
2025-11-30 13:19:06 +0100

    Modificado mensaje principal en la rama main

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@
  */
 public class Main {
         public static void main(String[] args) {
-        System.out.println("Hola mundo con Git en NetBeans!");
+        System.out.println("Hola desde MAIN en NetBeans!");
     }
 
 }
